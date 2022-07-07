07915e9e639ff0b74f074e13535eaabbb65ea1f1
John-Arboleda <johnjack0126@gmail.com>
2022-07-07 15:28:03 -0500

    Fix validation for reservation end_date

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,8 +1,14 @@
 class Reservation < ApplicationRecord
+  before_save :end_date_validation 
+
   belongs_to :user
   belongs_to :provider
 
   validates :start_date, presence: true, date: { after: Proc.new { Time.now }, before: Proc.new { Time.now + 6.months } }
   validates :end_date, presence: true, date: { after: Proc.new { :start_date } }
   validates :total_cost, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+  def end_date_validation
+    throw :abort unless self.end_date <= self.start_date + 1.month
+  end
 end
