4fa5d0e3a3a15215f7675990909e29753be22b05
Victor Peralta <victor.peralta.gomez@gmail.com>
2022-07-07 16:04:07 -0500

    Fix linter errors
    modified:   .rubocop.yml
    modified:   app/models/reservation.rb
    modified:   config/database.yml

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,16 +1,18 @@
 class Reservation < ApplicationRecord
-  before_save :end_date_validation 
+  before_save :end_date_validation
 
   belongs_to :user
   belongs_to :provider
 
-  validates :start_date, presence: true, date: { after: Proc.new { Time.now }, before: Proc.new { Time.now + 6.months } }
-  validates :end_date, presence: true, date: { after: Proc.new { :start_date } }
+  validates :start_date, presence: true, date: { after: proc { Time.now }, before: proc {
+                                                                                     Time.now + 6.months
+                                                                                   } }
+  validates :end_date, presence: true, date: { after: proc { :start_date } }
   validates :total_cost, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
   private
-  
+
   def end_date_validation
-    throw :abort unless self.end_date <= self.start_date + 1.month
+    throw :abort unless end_date <= start_date + 1.month
   end
 end
