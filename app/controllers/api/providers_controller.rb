4f9b723773021865c58208b3903513567dd05d58
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-07-08 19:34:43 -0300

    Add providers endpoints and routes

---
+++ app/controllers/api/providers_controller.rb
@@ -0,0 +1,69 @@
+class Api::ProvidersController < ApplicationController
+  # GET /api/providers
+  def index
+    render json: set_providers.to_json
+  end
+
+  # GET /api/providers/1
+  def show
+    render json: set_provider.to_json
+  end
+
+  # POST /api/providers
+  def create
+    @provider = Provider.new(provider_params)
+    if @provider.save
+      render json: { message: 'Provider created' }
+    else
+      render json: @provider.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /api/providers/1
+  def destroy
+    @provider = Provider.find(params[:id])
+    if @provider.destroy
+      render json: { message: 'Provider deleted' }
+    else
+      render json: @provider.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_providers
+    @providers = []
+    Provider.all.each do |provider|
+      @providers.push({
+                        id: provider.id,
+                        name: provider.name,
+                        bio: provider.bio,
+                        cost: provider.cost,
+                        image: url_for(provider.image),
+                        github_profile: provider.github_profile,
+                        linkedin_profile: provider.linkedin_profile,
+                        twitter_profile: provider.twitter_profile
+                      })
+    end
+    @providers
+  end
+
+  def set_provider
+    provider = Provider.find(params[:id])
+    {
+      id: provider.id,
+      name: provider.name,
+      bio: provider.bio,
+      cost: provider.cost,
+      image: url_for(provider.image),
+      github_profile: provider.github_profile,
+      linkedin_profile: provider.linkedin_profile,
+      twitter_profile: provider.twitter_profile
+    }
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def provider_params
+    params.permit(:name, :bio, :cost, :image, :github_profile, :linkedin_profile, :twitter_profile)
+  end
+end
--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,10 +1,16 @@
 class Provider < ApplicationRecord
+  include Rails.application.routes.url_helpers
+
   has_many :reservations
   has_many :skills
-  has_one_attached :avatar
+  has_one_attached :image
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :bio, presence: true
   validates :cost, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :image, presence: true
+
+  def image_url
+    url_for(image)
+  end
 end
