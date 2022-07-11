f2d8c1e20d01c2fd923999a0df4b23e868ea8e92
John-Arboleda <johnjack0126@gmail.com>
2022-07-11 14:51:16 -0500

    Add and Delete reservation actions

--- app/controllers/api/reservations_controller.rb
+++ app/controllers/api/reservations_controller.rb
@@ -1,25 +1,49 @@
 class Api::ReservationsController < ApplicationController
-  # GET api/reservation
-  def index
+  
+  def get_user
     result = UsersHelper::Validator.valid_user_token?(request.headers['Authorization'])
-    # result = UsersHelper::Validator.valid_user_token?('Bearer eyJhbGciOiJIUzI1NiJ9.bnVsbA.vdmTmedbjb6wZjzwL-u9n3uU447A_T1wQNzbYO6Doa8')
     unless result[0]
-      render json: '{"error":"Invalid user"}'
-      return
+      return false
     end
-    user = result[3]
-    reservations = user.reservations.all
-    render json: reservations
+    return result[3]
+  end
+  # GET api/reservations
+  def index
+    @user = self.get_user
+    if @user
+      reservations = @user.reservations.all
+      render json: reservations
+    else
+      render json: '{"error":"Invalid user"}'
+    end
   end
 
-  # POST api/reservation
+  # POST api/reservations
   def create
-    @reservation = @current_user.reservations.new(reservation_params)
-    @reservation.user_id = current_user
+    @user = self.get_user
+    @reservation = @user.reservations.new(reservation_params)
+    @reservation.user_id = @user.id
     @reservation.provider_id = params[:provider_id]
 
     if @reservation.save
       render json: @reservation, status: :created
+    else
+      render json: @reservation.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE api/reservations
+  def destroy
+    @user = self.get_user
+    @reservation = Reservation.find(params[:id])
+    
+    unless @user.id == @reservation.user_id
+      render json: { message: 'Invalid User' }
+      return
+    end
+
+    if @reservation.destroy
+      render json: { message: 'Reservation deleted' }
     else
       render json: @reservation.errors, status: :unprocessable_entity
     end
