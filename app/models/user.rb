6dba7eddb4de0862f360e4261ce39ea45ca24e5b
John-Arboleda <johnjack0126@gmail.com>
2022-07-12 10:12:02 -0500

    Add paswords_controller, fix rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,19 +10,19 @@ class User < ApplicationRecord
     self.reset_password_sent_at = Time.now.utc
     save!
   end
-   
+
   def password_token_valid?
-    (self.reset_password_sent_at + 4.hours) > Time.now.utc
+    (reset_password_sent_at + 4.hours) > Time.now.utc
   end
-   
+
   def reset_password!(password)
     self.reset_password_token = nil
     self.password = password
     save!
   end
-   
+
   private
-   
+
   def generate_token
     SecureRandom.hex(10)
   end
