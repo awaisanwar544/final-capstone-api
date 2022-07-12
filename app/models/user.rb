c268b6c60db7786fc45b209ebd09acc68cf6b415
John-Arboleda <johnjack0126@gmail.com>
2022-07-12 10:10:50 -0500

    Add routes and model methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,27 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
 
+  def generate_password_token!
+    self.reset_password_token = generate_token
+    self.reset_password_sent_at = Time.now.utc
+    save!
+  end
+   
+  def password_token_valid?
+    (self.reset_password_sent_at + 4.hours) > Time.now.utc
+  end
+   
+  def reset_password!(password)
+    self.reset_password_token = nil
+    self.password = password
+    save!
+  end
+   
   private
+   
+  def generate_token
+    SecureRandom.hex(10)
+  end
 
   def add_token
     self.token = JwtHelper::JsonWebToken.encode(email)
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,15 @@ Rails.application.routes.draw do
   post 'api/user', to: 'users#authenticate', as: 'authentication_user'
   post 'api/user/add', to: 'users#add', as: 'add_user_path'
   put 'api/user', to: 'users#change_password', as: 'change_password'
-  
+  # Password recovery
+  post 'api/password/forgot', to: 'passwords#forgot'
+  post 'api/password/reset', to: 'passwords#reset'
+
   namespace :api, defaults: { format: :json } do
     # Provider routes
     resources :providers, only: [:index, :show, :create, :destroy]
     # Reservation routes
-    resources :reservations, only: [:index, :show, :create, :destroy]
+    resources :reservations, only: [:index, :create, :destroy]
   end
-  # match '*unmatched', to: 'application#route_not_found', via: :all
+  match '*unmatched', to: 'application#route_not_found', via: :all
 end
